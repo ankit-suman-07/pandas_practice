e4e592fac7793af02d9d9fc68b877550451f612a
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-04-23 19:16:42 +0530

    Method chaining

---
+++ 2891_Method Chaining.py
@@ -0,0 +1,18 @@
+# DataFrame animals
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | name        | object |
+# | species     | object |
+# | age         | int    |
+# | weight      | int    |
+# +-------------+--------+
+# Write a solution to list the names of animals that weigh strictly more than 100 kilograms.
+
+# Return the animals sorted by weight in descending order.
+
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    result = animals[animals['weight'] > 100].sort_values(by='weight', ascending=False)
+    return result[['name']]
